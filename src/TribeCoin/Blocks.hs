ffa3a2e2ccf6e0830cc75b8e1e9e0051a6cb441d
RyanBeatty <rvbeatty@email.wm.edu>
2017-12-11 18:12:03 -0800

    added function to lazily calculate all of the prefixes for a block header

--- src/TribeCoin/Blocks.hs
+++ src/TribeCoin/Blocks.hs
@@ -9,6 +9,12 @@ import Data.ByteString.Conversion (toByteString')
 import Data.Serialize (encode)
 import Crypto.Hash (hash)
 
+-- | Hash a block header using sha256.
+hashBlockHeader ::
+  BlockHeader  -- ^ The block header to hash using sha256.
+  -> BlockHash
+hashBlockHeader = BlockHash . hash . encode
+
 -- | Lazy list of all possible nonce values.
 -- TODO: check if this is the right bounds. Also see if this gets garbage collected ever.
 -- If this is strictly evaluated and then sticks around in memory, We'll run out of memory real quick.
@@ -21,10 +27,12 @@ blocks ::
   -> [BlockHeader]
 blocks block_header = map (\n -> block_header { _nonce = n }) nonces
 
-hashBlockHeader ::
-  BlockHeader  -- ^ The block header to hash using sha256.
-  -> BlockHash
-hashBlockHeader = BlockHash . hash . encode
+-- | Returns a lazy list of all of the prefixes for all possible hashes of a block header.
+blockHashPrefixes ::
+  BlockHeader -- ^ The block header to hash.
+  -> Int      -- ^ The lenght of the hash prefix we want.
+  -> [BS.ByteString]
+blockHashPrefixes block_header  difficulty = map (BS.take difficulty . encode . hashBlockHeader) .  blocks $ block_header
 
 tryMineBlock :: 
   BlockHeader -- ^ The block header to try hashing.
