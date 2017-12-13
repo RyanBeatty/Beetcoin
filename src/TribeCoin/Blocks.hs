445997ac8c7a9221b2bf5d3042f03bc477308983
RyanBeatty <rvbeatty@email.wm.edu>
2017-12-12 17:38:50 -0800

    Added the ability to calculate the proof of work for a block header '

--- src/TribeCoin/Blocks.hs
+++ src/TribeCoin/Blocks.hs
@@ -7,6 +7,8 @@ import TribeCoin.Types (Block (..), BlockHeader (..), BlockHash (..), Nonce (..)
 
 import Data.ByteString as BS (ByteString, take, replicate)
 import Data.ByteString.Conversion (toByteString')
+import Data.List (find)
+import Data.Maybe (fromJust)
 import Data.Serialize (encode)
 import Crypto.Hash (hash)
 
@@ -34,6 +36,18 @@ blockHashPrefixes ::
   -> Int      -- ^ The lenght of the hash prefix we want.
   -> [(Nonce, BS.ByteString)]
 blockHashPrefixes block_header  difficulty = (fmap . fmap) (BS.take difficulty . encode . hashBlockHeader) .  blocks $ block_header
+
+-- | Calculates the proof of work for a block header.
+mineBlock ::
+  BlockHeader -- ^ The block header without a proof of work.
+  -> BlockHeader
+mineBlock block_header =
+  let difficulty = fromIntegral . _difficulty $ block_header
+      prefix    = BS.replicate difficulty 0
+      -- Find a nonce with a resulting block header hash prefix that is equal to the string of leading 0's needed to satisfy the difficulty target.
+      -- TODO: Using fromJust is probably a bad idea here. Maybe use an error monad or something.
+      (nonce, _) = fromJust . find ((==) prefix . snd) . blockHashPrefixes block_header $ difficulty
+  in block_header { _nonce = nonce }
 
 tryMineBlock :: 
   BlockHeader -- ^ The block header to try hashing.
