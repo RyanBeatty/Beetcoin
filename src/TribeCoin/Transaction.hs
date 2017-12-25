fc6b6ce873f71fee5a29357beb938c9cf84331c3
RyanBeatty <rvbeatty@email.wm.edu>
2017-12-25 13:57:27 -0800

    More refactoring

--- src/TribeCoin/Transaction.hs
+++ src/TribeCoin/Transaction.hs
@@ -16,12 +16,17 @@ verifyTx tx tx_map = undefined
 verifySigScript :: SigScript -> PubKeyHash -> SigMsg -> Bool
 verifySigScript script hash msg =
   let is_valid_pubkey = verifyPubKey (_pubKey script) hash
-      is_valid_sig = ECC.verifySig (_unPubKey . _pubKey $ script) (_unSig . _sig $ script) (_unSigMsg msg) 
+      is_valid_sig = verifySig (_pubKey script) (_sig $ script) msg 
   in is_valid_pubkey && is_valid_sig
 
+-- | Verifies that a public key matches a hash of a public key.
 verifyPubKey :: PubKey -> PubKeyHash -> Bool
 verifyPubKey pubkey hash =
   -- Hash the public key by first hashing with sha256 and then with ripemd160.
   let hash' = PubKeyHash . ripemd160 . sha256 $ (encode pubkey :: BS.ByteString)
   in hash' == hash
+
+-- | Verifies that a signature is valid given a public key and a message to sign.
+verifySig :: PubKey -> Sig -> SigMsg -> Bool
+verifySig pubkey sig msg = ECC.verifySig (_unPubKey pubkey) (_unSig sig) (_unSigMsg msg)
 
