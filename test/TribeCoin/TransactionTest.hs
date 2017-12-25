ef05a2c828bb324ef3b1280b292677c3ea6de15f
RyanBeatty <rvbeatty@email.wm.edu>
2017-12-25 12:22:41 -0800

    Added more tests

--- test/TribeCoin/TransactionTest.hs
+++ test/TribeCoin/TransactionTest.hs
@@ -6,8 +6,16 @@ import TribeCoin.TestUtils (parsedPubKey, parsedPubKeyHash)
 import TribeCoin.Transaction (verifyPubKey)
 import TribeCoin.Types (PubKey (..))
 
+import Test.Tasty.Hspec (Spec, it, describe)
 import Test.Tasty.QuickCheck (Property, forAll, arbitrary, suchThat)
 
+
+spec_VerifyPubKey :: Spec
+spec_VerifyPubKey = do
+  describe "verifyPubKey Tests" $ do
+    -- Verify that a public key will hash to the correct public key hash.
+    it "Public Key matches Public Key Hash" $ do
+      verifyPubKey parsedPubKey parsedPubKeyHash == True
 
 -- | Verifies that a public key will never hash to a public key hash that it doesn not match.
 prop_pubKeyAndPubKeyHashMismatch :: Property
