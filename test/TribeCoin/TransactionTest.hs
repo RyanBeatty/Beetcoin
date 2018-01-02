535deb497416724733959efca6ca236c69983c65
RyanBeatty <rvbeatty@email.wm.edu>
2018-01-02 10:54:38 -0800

    added some comments.

--- test/TribeCoin/TransactionTest.hs
+++ test/TribeCoin/TransactionTest.hs
@@ -17,18 +17,20 @@ spec_VerifyPubKey = do
     it "Public Key matches Public Key Hash" $ do
       verifyPubKey parsedPubKey parsedPubKeyHash == True
 
+-- TODO: Maybe turn this into a quickcheck test where a random SigMsg is generated
+-- and then signed by a priv key.
 spec_VerifySig :: Spec
 spec_VerifySig = do
   describe "verifySig Tests" $ do
+    -- Verify that a signature that was signed by a private key can be verified with the
+    -- original unsigned message and corresponding public key.
     it "PubKey validates signed message" $ do
       verifySig parsedPubKey parsedSig parsedSigMsg == True
 
 -- | Verifies that a public key will never hash to a public key hash that it doesn not match.
-prop_pubKeyAndPubKeyHashMismatch :: Property
-prop_pubKeyAndPubKeyHashMismatch =
+prop_PubKeyAndPubKeyHashMismatch :: Property
+prop_PubKeyAndPubKeyHashMismatch =
   -- Make sure we generate a public key hash that does not match the hash of this public
   -- key.
   forAll (arbitrary `suchThat` \h -> (_unRandomPubKeyHash h) /= parsedPubKeyHash) $
     \hash -> verifyPubKey parsedPubKey (_unRandomPubKeyHash hash) == False
-
-
