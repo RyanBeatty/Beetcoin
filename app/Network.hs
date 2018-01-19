56c6e209c08ada5554c0f9d37f7c75a3a27e2b12
RyanBeatty <rvbeatty@email.wm.edu>
2018-01-18 17:59:34 -0800

    More networking code

--- app/Network.hs
+++ app/Network.hs
@@ -1,6 +1,11 @@
 module Network where
 
-import Network.Transport (Transport (..), EndPoint (..))
+import qualified Data.ByteString as BS (ByteString (..))
+import qualified Data.ByteString.Char8 as BS8 (pack)
+import Network.Transport
+  ( Transport (..), EndPoint (..), Reliability (..), Connection (..), EndPointAddress (..)
+  , defaultConnectHints
+  )
 import Network.Transport.TCP (createTransport, defaultTCPParameters)
 
 data Node = Node
@@ -10,7 +15,10 @@ data Node = Node
   }
 
 instance Show Node where
-  show (Node _ endpoint _) = "Node: " ++ (show . address $ endpoint) 
+  show (Node _ endpoint _) = "Node: " ++ (show . address $ endpoint)
+
+makeEndPointAddress :: String -> String -> EndPointAddress
+makeEndPointAddress host port = EndPointAddress . BS8.pack $ host ++ ":" ++ port ++ ":" ++ "0"
 
 makeBeetCoinTransport :: String -> String -> IO (Transport)
 makeBeetCoinTransport host port = do
@@ -31,6 +39,21 @@ makeNode host port = do
   transport <- makeBeetCoinTransport host port
   endpoint <- makeBeetCoinEndPoint transport
   return $ Node transport endpoint (closeEndPoint endpoint >> closeTransport transport)
+
+connectToNode :: Node -> EndPointAddress -> IO (Connection)
+connectToNode node peer_address = do
+  conn <- connect (_endpoint node) peer_address ReliableOrdered defaultConnectHints
+  case conn of
+    Left _      -> undefined
+    Right conn' -> return conn'
+
+sendMessage :: Connection -> BS.ByteString -> IO ()
+sendMessage conn msg = do
+  result <- send conn [msg]
+  case result of
+    Left _  -> undefined
+    Right _ -> return ()
+    
 
 setupNetwork :: IO ()
 setupNetwork = undefined
